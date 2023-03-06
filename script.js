881436506f373270ba98483718645decb4b77272
Beloved Adejuyigbe <64827432+beladactions@users.noreply.github.com>
2023-03-06 04:34:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColor() {
+    var body = document.querySelector('body');
+    body.style.backgroundColor = getRandomColor();
+  }
+  
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
